9b6ca2f93faf406668f96759306dd3672c0e2756
Jefferson Henrique <jeffersonpi@hotmail.com>
2014-06-14 11:35:30 -0300

    - Funcionamento inicial do pso

---
+++ js/main.js
@@ -0,0 +1,199 @@
+var PARTICLE_VELOCITY = 300;
+
+$(document).ready(function(){
+	$(".glyphicon-info-sign").popover({trigger: "hover"});
+	
+	var btnExecute = $("#btnExecute");
+	
+	var panelConfigurations = $("#panel-configurations").find(".panel-body");
+	var panelResult = $("#panel-result");
+	var panelResultConfig = $(".panel-result-config");
+	
+	$("#resultCanvas").attr("width", panelResult.find(".panel-body").width() + "px");
+	$("#resultCanvas").attr("height", (panelConfigurations.outerHeight() - panelResultConfig.outerHeight()) + "px");
+	
+	var stage = new createjs.Stage("resultCanvas");
+	var containerParticles = new createjs.Container();
+	stage.addChild(containerParticles);
+	
+	var mouseDownPosition, previousX, previousY;
+	
+	$("#resultCanvas").on("mousedown", function(event) {
+		mouseDownPosition = [event.screenX, event.screenY];
+		
+		previousX = stage.x;
+		previousY = stage.y;
+	}).on("mouseup", function(event) {
+		mouseDownPosition = null;
+	}).on("mousemove", function(event) {
+		if (mouseDownPosition) {
+			stage.x = previousX + event.screenX - mouseDownPosition[0];
+			stage.y = previousY + event.screenY - mouseDownPosition[1];
+		}
+	});
+	
+	var psoSystem = null;
+	var resultPso = null;
+	var numberOfIterations = 0;
+	var allParticles = [];
+	
+	var diffX = null;
+	var diffY = null;
+	var restrictionPositions = [];
+	
+	function fixPosition(position) {
+		var x = (position[0] - restrictionPositions[0][0]) * diffX;
+		var y = (position[1] - restrictionPositions[1][0]) * diffY;
+		
+		return {x: x, y: y};
+	};
+	
+	btnExecute.click(function() {
+		allParticles.length = 0;
+		containerParticles.removeAllChildren();
+		
+		var numberOfParticles = parseInt($("#data-particles").val());
+		numberOfIterations = parseInt($("#data-iterations").val());
+		var numberOfIntervals = 5;
+		var formula = $("#data-formula").val();
+		var compCognitive = parseFloat($("#data-comp-cognitive").val());
+		var compSocial = parseFloat($("#data-comp-social").val());
+		var restPosition = $("#data-restriction-position").val();
+		var psoType = $("#data-psotype").val();
+		
+		restrictionPositions.length = 0;
+		var splitted = restPosition.trim().split(";");
+		for (var index = 0; index < splitted.length; index++) {
+			var values = splitted[index].split(",");
+			if (values.length == 2) {
+				restrictionPositions.push([parseFloat(values[0]), parseFloat(values[1])]);
+			}
+		}
+		
+		diffX = stage.canvas.width / (restrictionPositions[0][1] - restrictionPositions[0][0]);
+		diffY = stage.canvas.height / (restrictionPositions[1][1] - restrictionPositions[1][0]);
+		
+		psoSystem = new PSOSystem(numberOfParticles, numberOfIntervals, compCognitive, compSocial, restrictionPositions, formula, null);
+		resultPso = psoSystem.execute(numberOfIterations, psoType);
+		console.log(resultPso);
+		
+		for (var index = 0; index < (numberOfParticles+1); index++) {
+			var circle = new createjs.Shape();
+			if (index < numberOfParticles) {
+				circle.graphics.beginStroke("black").beginFill("white").drawCircle(0, 0, 15);
+			} else {
+				circle.graphics.beginStroke("white").beginFill("blue").drawCircle(0, 0, 15);
+			}
+			
+			var positionFixed = fixPosition(resultPso[index][0]);
+			circle.x = positionFixed.x;
+			circle.y = positionFixed.y;
+			circle._nextNode = 1;
+			circle._positions = resultPso[index];
+			
+			allParticles.push(circle);
+			containerParticles.addChild(circle);
+		}
+	});
+	
+	function tick(event) {
+		if (event.paused) {
+			return;
+		}
+		
+		var allHaveFinished = allParticles.length > 0;
+		for (var index in allParticles) {
+			var currentParticle = allParticles[index];
+
+			if (currentParticle._nextNode < numberOfIterations) {
+				allHaveFinished = false;
+				
+				var currentState = resultPso[index][currentParticle._nextNode - 1].slice(0);
+				var positionFixed = fixPosition(currentState);
+				currentState[0] = positionFixed.x;
+				currentState[1] = positionFixed.y;
+				
+				var nextState = resultPso[index][currentParticle._nextNode].slice(0);
+				positionFixed = fixPosition(nextState);
+				nextState[0] = positionFixed.x;
+				nextState[1] = positionFixed.y;
+				
+				
+				var diffX = nextState[0] - currentParticle.x;
+				var diffY = nextState[1] - currentParticle.y;
+				var distance = Math.sqrt(diffX*diffX + diffY*diffY);
+				
+				var auxDistance = distance;
+				if (auxDistance < 1) {
+					diffX = nextState[0] - currentState[0];
+					diffY = nextState[1] - currentState[1];
+					distance = Math.sqrt(diffX*diffX + diffY*diffY);
+					auxDistance = Math.sqrt(diffX*diffX + diffY*diffY);
+				}
+				
+				var angleRadians = Math.asin(diffY / auxDistance);
+				if (Number.isNaN(angleRadians)) {
+					angleRadians = 0;
+				}
+				
+				if (nextState[0] < currentState[0]) {
+					angleRadians = Math.PI - angleRadians;
+				}
+				
+				var angleDegree = angleRadians * (180/Math.PI);
+				var cosAngle = Math.cos(angleRadians);
+				var sinAngle = Math.sin(angleRadians);
+				
+				var amountX = event.delta/1000*PARTICLE_VELOCITY * cosAngle;
+				if (Math.abs(amountX) < 0.001) {
+					amountX = 0;
+				}
+				
+				var amountY = event.delta/1000*PARTICLE_VELOCITY * sinAngle;
+				if (Math.abs(amountY) < 0.001) {
+					amountY = 0;
+				}
+				
+				if (
+						(amountX != 0 && 
+								(
+								(currentParticle.x <= nextState[0] && nextState[0] <= (currentParticle.x + amountX))
+										 || 
+								(currentParticle.x >= nextState[0] && nextState[0] >= (currentParticle.x + amountX))
+								)
+						) ||
+						(amountY != 0 && 
+								(
+								(currentParticle.y >= nextState[1] && nextState[1] >= (currentParticle.y + amountY))
+										||
+								(currentParticle.y <= nextState[1] && nextState[1] <= (currentParticle.y + amountY))
+								)
+						)
+					) {
+					currentParticle.x = nextState[0];
+					currentParticle.y = nextState[1];
+					
+					currentParticle._nextNode++;
+					
+					if (index == allParticles.length - 1) {
+						console.log(currentParticle.x + " - " + currentParticle.y);
+					}
+				} else {
+					currentParticle.x += amountX;
+					currentParticle.y += amountY;
+					
+					currentParticle.rotation = angleDegree;
+				}
+			}
+		}
+		
+		if (allHaveFinished) {
+		}
+		
+		stage.update(event);
+	}
+	
+	createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCH;
+	createjs.Ticker.framerate = 30;
+	createjs.Ticker.addEventListener("tick", tick);
+});
--- js/pso.js
+++ js/pso.js
@@ -12,7 +12,7 @@ function PSOParticle(pso) {
 		var result = pso.evaluateFormula(this.position);
 		
 		if (result < this.bestLocalValue) {
-			this.bestPosition = this.positions.slice(0);
+			this.bestPosition = this.position.slice(0);
 			this.bestLocalValue = result;
 		}
 		
@@ -21,7 +21,7 @@ function PSOParticle(pso) {
 	
 }
 
-function PSOSystem(numberOfParticles, numberOfIntervals, cCognitive, cSocial, positionRestriction) {
+function PSOSystem(numberOfParticles, numberOfIntervals, cCognitive, cSocial, positionRestriction, expressionFormula, psoTypeParams) {
 	
 	this.positionRestriction = positionRestriction;
 	this.velocityRestriction = [];
@@ -29,22 +29,31 @@ function PSOSystem(numberOfParticles, numberOfIntervals, cCognitive, cSocial, po
 	this.componentCognitive = cCognitive;
 	this.componentSocial = cSocial;
 	
+	this.psoTypeParams = psoTypeParams;
+	
 	this.particles = [];
 	
 	this.bestPosition = [];
 	this.bestValue = Number.MAX_VALUE;
+	
+	this.resultStep = [];
+	
+	this.expression = Parser.parse(expressionFormula); 
 	
 	for (var index = 0; index < numberOfParticles; index++) {
 		var p = new PSOParticle(this);
 		for (var indexX = 0; indexX < positionRestriction.length; indexX++) {
 			p.position.push(0);
 			p.velocity.push(0);
+			p.bestPosition.push(0);
 		}
 		
 		this.particles.push(p);
 	}
 	
 	for (var index = 0; index < positionRestriction.length; index++) {
+		this.bestPosition.push(0);
+		
 		var v1 = (positionRestriction[index][0] - positionRestriction[index][1]) / numberOfIntervals;
 		var v2 = (positionRestriction[index][1] - positionRestriction[index][0]) / numberOfIntervals;
 		
@@ -52,6 +61,7 @@ function PSOSystem(numberOfParticles, numberOfIntervals, cCognitive, cSocial, po
 	}
 	
 	this.init = function() {
+		this.resultStep.length = 0;
 		this.bestValue = Number.MAX_VALUE;
 	
 		for (var indexP = 0; indexP < this.particles.length; indexP++) {
@@ -60,28 +70,89 @@ function PSOSystem(numberOfParticles, numberOfIntervals, cCognitive, cSocial, po
 				this.particles[indexP].velocity[indexR] = this.velocityRestriction[indexR][0] + Math.random() * (this.velocityRestriction[indexR][1] - this.velocityRestriction[indexR][0]);
 			}
 			
+			this.resultStep.push([this.particles[indexP].position.slice(0)]);
+			
 			var resultParticle = this.particles[indexP].evaluate();
 			if (resultParticle < this.bestValue) {
-				this.bestPosition = this.particles[indexP].slice(0);
+				this.bestPosition = this.particles[indexP].position.slice(0);
 				this.bestValue = resultParticle;
 			}
 		}
+		
+		this.resultStep.push([this.bestPosition.slice(0)]);
 	};
 	
-	this.execute = function(maxSteps) {
+	this.evaluateFormula = function(positions) {
+		var params = {x: positions[0], y: positions[1]};
+		
+		return this.expression.evaluate(params);
+	};
+	
+	this.execute = function(maxSteps, psoType) {
 		var counter = 0;
 		
-		init();
+		this.init();
 		
 		while (counter < maxSteps) {
 			counter++;
 			
 			for (var indexP = 0; indexP < this.particles.length; indexP++) {
 				for (var indexR = 0; indexR < this.positionRestriction.length; indexR++) {
+					if (psoType == "DEFAULT") {
+						this.particles[indexP].velocity[indexR] = this.particles[indexP].velocity[indexR] + 
+							this.componentCognitive * Math.random() * (this.particles[indexP].bestPosition[indexR] - this.particles[indexP].position[indexR]) + 
+							this.componentSocial * Math.random() * (this.bestPosition[indexR] - this.particles[indexP].position[indexR])
+					} else if (psoType == "WEIGHT_LINEAR_DESC") {
+						var weightMin = this.psoTypeParams["WEIGHT_MIN"];
+						var weightMax = this.psoTypeParams["WEIGHT_MAX"];
+						
+						var weight = weightMax - ((weightMax - weightMin) / maxSteps) * counter;
+						
+						this.particles[indexP].velocity[indexR] = weight * this.particles[indexP].velocity[indexR] + 
+							this.componentCognitive * Math.random() * (this.particles[indexP].bestPosition[indexR] - this.particles[indexP].position[indexR]) + 
+							this.componentSocial * Math.random() * (this.bestPosition[indexR] - this.particles[indexP].position[indexR])
+					} else if (psoType == "CONSTRICTION") {
+						this.particles[indexP].velocity[indexR] = this.particles[indexP].velocity[indexR] + 
+							this.componentCognitive * Math.random() * (this.particles[indexP].bestPosition[indexR] - this.particles[indexP].position[indexR]) + 
+							this.componentSocial * Math.random() * (this.bestPosition[indexR] - this.particles[indexP].position[indexR])
+						
+						this.particles[indexP].velocity[indexR] *= this.psoTypeParams["CONSTRICTION"];
+					}
+					
+					if (this.particles[indexP].velocity[indexR] < this.velocityRestriction[indexR][0]) {
+						this.particles[indexP].velocity[indexR] = this.velocityRestriction[indexR][0]
+					} else if (this.particles[indexP].velocity[indexR] > this.velocityRestriction[indexR][1]) {
+						this.particles[indexP].velocity[indexR] = this.velocityRestriction[indexR][1]
+					}
+					
+					this.particles[indexP].position[indexR] = this.particles[indexP].position[indexR] + this.particles[indexP].velocity[indexR];
+					
+					if (this.particles[indexP].position[indexR] < this.positionRestriction[indexR][0]) {
+						this.particles[indexP].position[indexR] = this.positionRestriction[indexR][0]
+					} else if (this.particles[indexP].position[indexR] > this.positionRestriction[indexR][1]) {
+						this.particles[indexP].position[indexR] = this.positionRestriction[indexR][1]
+					}
 					
 				}
+				
+				this.resultStep[indexP].push(this.particles[indexP].position.slice(0));
+				
+				var resultParticle = this.particles[indexP].evaluate();
+				if (resultParticle < this.bestValue) {
+					this.bestPosition = this.particles[indexP].position.slice(0);
+					this.bestValue = resultParticle;
+				}
+				
 			}
+			
+			this.resultStep[this.resultStep.length-1].push(this.bestPosition.slice(0));
 		}
+		
+		return this.resultStep;
 	};
-	
+}
+
+function test() {
+	var pso = new PSOSystem(8, 5, 2, 2, [[-6, 8], [-10, 7]], "x^2 - x*y + y^2 - 3*y", null);
+	console.log(pso.execute(150, "DEFAULT"));
 }
